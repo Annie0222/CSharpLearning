ada75890f5f8067311c67f47138c325b0915c646
Annie <fangyuqian0222@gmail.com>
2019-07-06 20:32:09 +0800

    Add Oven

---
+++ Oven/Oven/Oven.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Oven
+{
+    class Oven
+    {
+        private int temperature = 37;
+        private int threshold;
+        private bool isClosed = false;
+        public delegate void OverHeatHandler(int temperature);
+        public event OverHeatHandler OverHeat; 
+
+        public Oven(int threshold)
+        {
+            this.threshold = threshold;
+        }
+
+        public void Start()
+        {
+            while (!isClosed)
+            {
+                Console.WriteLine($"Temperature of oven is {temperature}.");
+                Thread.Sleep(1000);
+                if (temperature > threshold) OverHeat(temperature);
+                temperature++;
+            }
+        }
+
+        public void Close()
+        {
+            isClosed = true;
+        }
+    }
+}
---
+++ Oven/Oven/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Oven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Oven oven = new Oven(100);
+            oven.OverHeat += new Oven.OverHeatHandler((temp) => Console.WriteLine($"Warning! Oven is over heat. Current temperature is {temp}."));
+            oven.OverHeat += new Oven.OverHeatHandler((temp) => {
+                Console.WriteLine("Close oven when temperature is over 102.");
+                if (temp > 102) oven.Close();
+            });
+            oven.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
